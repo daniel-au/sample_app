066a570155179261da7266785721bb37b69799f0
Daniel Au <audaniel94@gmail.com>
2019-06-06 11:11:01 -0700

    Added a full_title helper
    There is now a default title for all pages and then it adds a piece if the page
    has a specific name.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    full_title = "Ruby on Rails Tutorial Sample App"
+    full_title = page_title + " | " + full_title unless page_title.empty?
+    return full_title
+  end
 end
