dc19574dfef36d6ef2ec5f70faeacd17cd3c2167
Daniel Au <audaniel94@gmail.com>
2019-06-08 01:48:13 -0700

    Added Integration Tests
    Added site layout integration tests that test for the presence of specific html
    tags. Included a test for the full_title helper function, and refactored the
    code for the helper function.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
     full_title = "Ruby on Rails Tutorial Sample App"
-    full_title = page_title + " | " + full_title unless page_title.empty?
+    full_title = "#{page_title} | #{full_title}" unless page_title.empty?
     return full_title
   end
 end
